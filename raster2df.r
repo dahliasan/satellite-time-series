27782f26bf94960d8d7c67c15a8ab33fb347874a
oofd <dahliafoo@gmail.com>
2018-07-04 10:37:41 +1000

    reorganising

--- raster2df.r
+++
@@ -1,9 +0,0 @@
-raster2df <- function(x){
-  df <- data.frame(rasterToPoints(x))
-  t <- names(x)
-  t <- sapply(t, function(x) stringr::str_split(x, 'X')[[1]][2])
-  t <- as.character(gsub('\\.', '-', t))
-  colnames(df)[3:length(df)] <- t
-  df <- df %>% tidyr::gather("date", 'v1', 3:length(df))
-  return(df)
-}
